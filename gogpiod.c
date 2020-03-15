d12defbc29f97d34946cf7f87e0f9d87c27a526b
wancom <softpiware@gmail.com>
2020-03-15 14:53:00 +0900

    Refactor

--- gogpiod.c
+++ gogpiod.c
@@ -6,54 +6,10 @@ char *dev="";
 char *appname="gpiod";
 struct gpiod_chip *gchip=NULL;
 
-// static int poll_callback(unsigned int num_lines, struct gpiod_ctxless_event_poll_fd *fds, const struct timespec *timeout, void *data) {
-// 	struct pollfd pfds[GPIOD_LINE_BULK_MAX_LINES + 1];
-// 	int cnt;
-// 	unsigned int i;
-
-// 	for (i = 0; i < num_lines; i++) {
-// 		pfds[i].fd = fds[i].fd;
-// 		pfds[i].events = POLLIN | POLLPRI;
-// 	}
-
-// 	cnt = poll(pfds, num_lines, timeout->tv_sec * 1000 + timeout->tv_nsec / 1000000);
-
-// 	if (cnt < 0) return GPIOD_CTXLESS_EVENT_POLL_RET_ERR;
-// 	else if (cnt == 0) {
-//     if (checkStop()) return GPIOD_CTXLESS_EVENT_POLL_RET_STOP;
-//     return GPIOD_CTXLESS_EVENT_POLL_RET_TIMEOUT;
-//   }
-
-// 	for (i = 0; i < num_lines; i++) {
-// 		if (pfds[i].revents) fds[i].event = true;
-// 	}
-
-//   return cnt;
-// }
-
-// static int event_callback(int event_type, unsigned int line_offset, const struct timespec *timestamp, void *data) {
-//   int ret = 0;
-
-//   switch (event_type) {
-//     case GPIOD_CTXLESS_EVENT_CB_RISING_EDGE:
-//       ret = intGPIO(line_offset, 1 ,timestamp->tv_sec,timestamp->tv_nsec);
-//       break;
-//     case GPIOD_CTXLESS_EVENT_CB_FALLING_EDGE:
-//       ret = intGPIO(line_offset, 0 ,timestamp->tv_sec,timestamp->tv_nsec);
-//       break;
-//     default:
-//       return GPIOD_CTXLESS_EVENT_CB_RET_OK;
-// 	}
-// 	if (ret) return GPIOD_CTXLESS_EVENT_CB_RET_STOP;
-// 	return GPIOD_CTXLESS_EVENT_CB_RET_OK;
-// }
-// int watchGPIO(unsigned int *gpio,int cnt){
-// 	struct timespec timeout = { 1, 0 };
-//   return gpiod_ctxless_event_monitor_multiple(dev, GPIOD_CTXLESS_EVENT_BOTH_EDGES, gpio, cnt, false, appname, &timeout, poll_callback, event_callback, NULL);
-// }
-
 int watchGPIO(unsigned int gpio,int cnt){
 	struct gpiod_line *gl;
+	struct gpiod_line_event ge;
+	struct pollfd fds;
 	int fd,ret;
 
 	if (gchip == NULL){
@@ -61,33 +17,28 @@ int watchGPIO(unsigned int gpio,int cnt){
 		return -1;
 	}
 
-	if ((gl=gpiod_chip_get_line(gchip,gpio))==NULL){
+	if ((gl=gpiod_chip_get_line(gchip, gpio))==NULL){
 		return -1;
 	}
 
-	if (gpiod_line_request_both_edges_events(gl,appname)==-1){
+	if (gpiod_line_request_both_edges_events(gl, appname)==-1){
 		return -1;
 	}
 	if ((fd=gpiod_line_event_get_fd(gl))==-1){
 		return -1;
 	}
 
-	struct pollfd fds;
-
+	memset(&fds, 0, sizeof(fds));
 	fds.fd = fd;
 	fds.events = POLLIN | POLLPRI;
 
-	while(1){
+	while(!checkStop()){
 		cnt = poll(&fds, 1, 1*1000);
 
 		if (cnt < 0) return -1;
-		else if (cnt == 0) {
-			if (checkStop()) break;
-			continue;
-		}
+		else if (cnt == 0) continue;
 
 		if (fds.revents) {
-			struct gpiod_line_event ge;
 			if (gpiod_line_event_read(gl,&ge)==-1){
 				return -1;
 			}
